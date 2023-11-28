12dc8c6a64ae1ec18a7e39a1297f860247c98096
flaviogui <flavioglaydson-pc@hotmail.com>
2023-11-28 10:40:13 -0300

    OK

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function Home (){
+    return (
+    <div>
+        <h1>Site PWEB</h1>
+        <a class="uncolored-link" href='https://github.com/flaviogui/PWeb' target='blank'>GitHub</a>
+        <h3>Receitas NEXT</h3>
+        <a href='https://github.com/flaviogui/PWeb/tree/main/Receitas%202%C2%AA%20Unidade/pages/receita1' target='blank'>Receita 1</a><br></br>
+        <a href='https://github.com/flaviogui/PWeb/tree/main/Receitas%202%C2%AA%20Unidade/pages/receita2' target='blank'>Receita 2</a><br></br>
+    </div>
+    )
+}
