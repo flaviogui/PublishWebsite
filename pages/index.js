1a2c17a731a793cb707ef0ea910c99f77c1a4140
flaviogui <flavioglaydson-pc@hotmail.com>
2023-12-01 11:28:09 -0300

    Atualizando

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ export default function Home (){
         <h3>Receitas NEXT</h3>
         <a href='https://github.com/flaviogui/PWeb/tree/main/Receitas%202%C2%AA%20Unidade/pages/receita1' target='blank'>Receita 1</a><br></br>
         <a href='https://github.com/flaviogui/PWeb/tree/main/Receitas%202%C2%AA%20Unidade/pages/receita2' target='blank'>Receita 2</a><br></br>
-        <a href='https://github.com/flaviogui/PWeb/tree/main/Receitas%202%C2%AA%20Unidade/pages/receita3' target='blank'>Receita 2</a><br></br>
+        <a href='https://github.com/flaviogui/PWeb/tree/main/Receitas%202%C2%AA%20Unidade/pages/receita3' target='blank'>Receita 3</a><br></br>
+        <a href='https://github.com/flaviogui/PWeb/tree/main/Receitas%202%C2%AA%20Unidade/pages/receita4' target='blank'>Receita 4</a><br></br>
     </div>
     )
 }
