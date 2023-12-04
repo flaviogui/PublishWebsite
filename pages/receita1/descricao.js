920001dd004e94c14005d9e6146b1a163bdb62e6
flaviogui <flavioglaydson-pc@hotmail.com>
2023-12-04 11:22:49 -0300

    Verificar

--- pages/receita1/descricao.js
+++ pages/receita1/descricao.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export function Descricao() {
     return (
         <p>Um ex-policial militar, um agente do FBI e uma jovem usam o carro KITT, um Ford Shelby GT500KR dotado de inteligência artificial e equipado até com mísseis, para combater o crime.</p>
--- pages/receita2/sobre.js
+++ pages/receita2/sobre.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export function Imagem() {
     return (
         <img width='500' src="https://www.blogauto.com.br/wp-content/2009/05/knightrider-605x324.jpg" alt="Next.js symbol" />
