ecab6e14ea87a665ad16ed25eb688f995b41a45b
flaviogui <flavioglaydson-pc@hotmail.com>
2023-12-11 09:42:57 -0300

    Atualização 0.4

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,16 @@ export default function Home (){
     return (
     <div>
         <h1>Site PWEB</h1>
-        <a class="uncolored-link" href='https://github.com/flaviogui/PWeb' target='blank'>GitHub</a>
+        <a class="uncolored-link" href='https://github.com/flaviogui/PublishWebsite' target='blank'>GitHub</a>
         <h3>Receitas NEXT</h3>
         <h4>Receita 1</h4>
-        <p><a href='https://github.com/flaviogui/PWeb/tree/main/Receitas%202%C2%AA%20Unidade/pages/receita1' target='blank'>Código - Git Hub</a> / <a href="./receita1" target="blank">Receita no ar</a></p>
+        <p><a href='https://github.com/flaviogui/PublishWebsite/tree/main/pages/receita1' target='blank'>Código - Git Hub</a> / <a href="./receita1" target="blank">Receita no ar</a></p>
         <h4>Receita 2</h4>
-        <p><a href='https://github.com/flaviogui/PWeb/tree/main/Receitas%202%C2%AA%20Unidade/pages/receita2' target='blank'>Código Git Hub</a> / <a href="./receita2/novaPagina" target="blank">Receita no ar</a></p>
+        <p><a href='https://github.com/flaviogui/PublishWebsite/tree/main/pages/receita2' target='blank'>Código Git Hub</a> / <a href="./receita2/novaPagina" target="blank">Receita no ar</a></p>
         <h4>Receita 3</h4>
-        <p><a href='https://github.com/flaviogui/PWeb/tree/main/Receitas%202%C2%AA%20Unidade/pages/receita3' target='blank'>Código Git Hub</a> / <a href="./receita3/movies" target="blank">Receita no ar</a></p>
+        <p><a href='https://github.com/flaviogui/PublishWebsite/tree/main/pages/receita3' target='blank'>Código Git Hub</a> / <a href="./receita3/movies" target="blank">Receita no ar</a></p>
+        <h4>Receita 4</h4>
+        <p><a href='https://github.com/flaviogui/PublishWebsite/tree/main/pages/receita4' target='blank'>Código Git Hub</a> / <a href="./receita4/movies2" target="blank">Receita no ar</a></p>
     </div>
     )
 }
